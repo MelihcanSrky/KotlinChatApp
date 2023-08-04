0a4ec82be40325b0da2b233a67f75df612f0580f
Melihcan Sarıkaya <sarikayamelihcan-01@outlook.com>
2023-08-04 23:04:14 +0300

    create chat conflict update

--- chatapp-backend/src/controllers/chats/createChat.js
+++ chatapp-backend/src/controllers/chats/createChat.js
@@ -17,6 +17,19 @@ const createChat = async (req, res, next) => {
             [friend_uuid]
         );
 
+        const chatExists = await pool.query(
+            "SELECT COUNT(*) AS count FROM chats WHERE (user_uuid = $1 AND chatname = $4) OR (user_uuid = $2 AND chatname = $3)",
+            [user_uuid, friend_uuid, me.rows[0].username, friend.rows[0].username]
+        );
+
+        if (chatExists.rows[0].count > 0) {
+            return res.status(409).json({
+                status: 409,
+                message: 'chat already exists',
+                data: null
+            })
+        }
+
         const user = await pool.query(
             "INSERT INTO chats (chat_uuid, user_uuid, chatname, created_at) VALUES ($1, $2, $3, $4) RETURNING *",
             [chat_uuid, user_uuid, friend.rows[0].username, create_at]
